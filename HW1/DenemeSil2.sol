50d10c8074926ff6ad6b9019c28980c0ee798b44
ofenerci <fenerci.ozhan@gmail.com>
2015-09-14 00:50:24 +0300

    My First Commit

---
+++ HW1/DenemeSil2.sol
@@ -0,0 +1,16 @@
+\showAllAnsAtEnd
+\eqEXt\exerSolnHeader{Solutions to }{ex.1}{\llap{\textbf{1.\ }}}\relax 
+\begin{ssol}
+$ 2x + 5 = -2 \implies 2x = -7 \implies x = -7/2 $
+\end{ssol}
+\begin{lsol}
+Solve the equation $ 2x + 5 = -2 $ for $x$. Indeed,
+\begin{alignat*}{2}
+    2x + 5 &= -2 &&\qquad\text{given}\\
+        2x &= -7 &&\qquad\text{subtract $5$ both sides}\\
+         x &= \boxed{-\frac{7}{2}}
+            &&\qquad\text{divide by $3$ both sides}\\
+\end{alignat*}
+\end{lsol}
+\ReturnTo{page.1}{\hbox {$\square $}}\endeqEXt\par\medskip
+ 
